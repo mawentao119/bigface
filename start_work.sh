68715b6b1b8b401800130fb5c61457ad146ac50f
charisma <mawentao119@gmail.com>
2021-03-01 12:03:42 +0800

    add markdown

--- start_work.sh
+++ start_work.sh
@@ -2,19 +2,12 @@
 
 echo "*** Source py3env/bin/activate ... "
 
-source py3env/bin/activate
+source ../py3env/bin/activate
 
 here=`pwd`
 
 ulimit -n 4096
 
-cd work/workspace/Admin/darwen
-
-rm -rf auto/www/static/js/*
-rm -rf keyword/*
-
-  nohup   python darwen.py runserver -h 0.0.0.0 -p 8082  &
-
-cd ${here}
+nohup   python davinci.py runserver -h 0.0.0.0 -p 8082  &
 
 echo "*** Start finished ... "
